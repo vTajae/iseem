57b4076604d7835d913bee1cc133cc5cd42d19e2
Your Name <you@example.com>
2024-02-13 19:15:43 -0500

    Deployment Test

--- app/api/dependencies/database.py
+++ app/api/dependencies/database.py
@@ -41,7 +41,7 @@ class AsyncDatabaseSession:
         async with self.engine.begin() as conn:
             async with self.engine.begin() as conn:
                 # Drop all tables
-                await conn.run_sync(Base.metadata.drop_all)
+                # await conn.run_sync(Base.metadata.drop_all)
                 # Create all tables
                 await conn.run_sync(Base.metadata.create_all)
 
